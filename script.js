f8f37459c9eaaa6d6d00812ee494990799021ac2
Adrian Ionut Zaharia <addyionut@gmail.com>
2021-10-25 16:45:25 +0300

    Update script.js
    Created an animated rotation, but it stops on a previously set character.

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 const gameMsg = document.getElementById('msg');
 gameMsg.innerHTML = "Press the lucky button!";
-var boxes = ["", "", ""];
 
 function randomChars() {
     var characters = 'ABC';
     var charsLength = characters.length;
     return characters.charAt(Math.floor(Math.random() * charsLength));
 }
+function count() {
+  var letters = "ABC";
+  var allCounters = document.querySelectorAll(".counter > div");
 
-function playGame() {
-	for (let i = 1; i <= 3; ++i) {
-		var rnd = randomChars();
-		var div = document.getElementById(i);
-		div.innerHTML = rnd;
-		boxes[i - 1] = rnd;
-	}
-	win();
-}
-
-function win() {
-	if (boxes[0] === boxes[1] && boxes[1] === boxes[2]) {
-		gameMsg.innerHTML = "Congratulations! You won!";
-	}
-	else {
-		gameMsg.innerHTML = "Play again!";
-	}
+  allCounters.forEach(function(el) {
+    var duration = 1000 + Array.from(allCounters).indexOf(el) * 1000;        
+    var interval = setInterval(function() {
+      el.innerText = letters.charAt(Math.random() * letters.length);
+      duration = duration - 50;
+      if (duration <= 0) {
+        clearInterval(interval);
+        el.innerText = el.getAttribute("data-final");
+      }
+    }, 50);
+  });
 }
